20a54e7b5ed4d52d0969638444b4d8ff8b313a18
Jacob Chen <cwj9658@gmail.com>
2023-11-03 19:58:48 +0800

    add working experience module

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import SocialLink from "@/app/ui/social-link";
+import About from "@/app/ui/about";
+import Experience from "@/app/ui/experience";
 
 export default function Home() {
     return (
-        <div className="min-h-screen mx-auto md:px-12 lg:px-24 lg:py-0 px-6 py-12">
+        <div className="min-h-screen mx-auto max-w-screen-xl md:px-12 lg:px-24 lg:py-0 px-6 py-12">
             <div className="lg:flex lg:justify-between lg:gap-4">
                 <div
-                    className="lg:flex lg:sticky lg:top-0 lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 lg:min-h-screen">
+                    className="lg:flex lg:sticky lg:top-0 lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 lg:max-h-screen">
                     <div>
                         <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">Jacob Chen</h1>
                         <h2 className="text-lg font-medium mt-3 tracking-tight sm:text-xl">Fullstack Engineer</h2>
@@ -16,8 +18,8 @@ export default function Home() {
                 </div>
 
                 <div className="pt-24 lg:w-1/2 lg:py-24">
-                    <p className="mb-16 md:mb-24 lg:mb-36">self introduction</p>
-                    <p className="mb-16 md:mb-24 lg:mb-36 whitespace-normal break-words">working experience</p>
+                    <About/>
+                    <Experience/>
                     <p className="mb-16 md:mb-24 lg:mb-36">projects</p>
                     <footer className="max-w-md pb-16 text-sm sm:pb-0">
                         <p>Coded in Jetbrains WebStorm by yours truly. Built with Next.js and Tailwind CSS based on
---
+++ app/ui/about.tsx
@@ -0,0 +1,32 @@
+export default function About() {
+    return (
+        <div className="mb-16 md:mb-24 lg:mb-36">
+            <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 md:-mx-12 md:px-12
+            lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">About</h2>
+            </div>
+            <p className="mb-4">
+                Back in 2015, I was just a low-voltage electrical engineer working with PLC programs.
+                It was at the urging of my friend that I first tumbled head into the rabbit hole of coding and web
+                development.
+                Fast forward to today, I've had the privilege of building software for a <span
+                className="font-medium">start-up</span>
+                , an <a className="font-medium" href="https://www.manulife.com/" target="_blank">international financial
+                institution</a>,
+                and a <a className="font-medium" href="https://english.cmbchina.com/" target="_blank">huge banking
+                corporation</a>.
+            </p>
+            <p className="mb-4">
+                My main focus these days is building SaaS enterprise products. In my free time I explore different
+                technology stack.
+            </p>
+            <p className="mb-4">
+                When I'm not at the computer, I'm usually taking leisurely strolls with my partner, indulging in diverse
+                cuisines,
+                or tackling endless traffic jam in <a className="font-medium"
+                                                      href="https://www.paradoxinteractive.com/games/cities-skylines-ii/about"
+                                                      target="_blank">cities</a>.
+            </p>
+        </div>
+    )
+}
